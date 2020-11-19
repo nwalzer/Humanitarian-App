d7a06f188e2765877db614441f170d802e7f2e6d
nwalzer <nswalzer@gmail.com>
2020-11-19 17:16:19 -0500

    Added paths

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,21 @@ app.get('/', function(req, res) {
 	res.send('<h1>Welcome to Node.js project setup</h1>')
 })
 
+app.post('/register', function(req, res){
+	//TODO: implement
+	res.send('Success')
+})
+
+app.post('/login', function(req, res){
+	//TODO: implement
+	res.send('Success')
+})
+
+app.post('/review', function(req, res){
+	//TODO: implement
+	res.send('Success')
+})
+
 app.listen(3000, function(){
 	console.log("Server started on port: 3000")
 })
