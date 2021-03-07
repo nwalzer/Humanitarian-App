fdc5b1b9b8c54211c3b2af6339b2b53da49adf60
nwalzer <nswalzer@gmail.com>
2021-03-07 14:57:12 -0500

    Resource info is passed to dialog when clicked

--- frontend/src/components/ResourceList.jsx
+++ frontend/src/components/ResourceList.jsx
@@ -2,11 +2,47 @@ import React, { useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { List, ListItem } from '@material-ui/core';
+import { List, ListItem, Dialog, DialogTitle, Button } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { useHistory  } from 'react-router-dom'
+
+
+function SimpleDialog(props) {
+    const { onClose, open, docInfo } = props;
+
+    console.log(docInfo);
+
+    return (
+        <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
+            <DialogTitle id="simple-dialog-title">Resource Information</DialogTitle>
+            info goes here
+            <Button> Leave a Review </Button>
+        </Dialog>
+    );
+}
+
+SimpleDialog.propTypes = {
+    onClose: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
+    docInfo: PropTypes.object.isRequired
+};
 
 export default function ResourceList(){
+    const [open, setOpen] = useState(false);
     const [user, setUser] = useState(false);
     const [dbData, setDBData] = useState();
+    const [selectedData, setSelectedData] = useState();
+    let history = useHistory();
+
+    const handleClick = (doc) => {
+        console.log(doc);
+        setOpen(true);
+        setSelectedData(doc);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     firebase.auth().onAuthStateChanged(userStatus => {
         if (userStatus && !user) {
@@ -17,6 +53,7 @@ export default function ResourceList(){
                     console.log(doc.data());
                     let thisData = doc.data();
                     let tempInfo = {
+                        "uid":doc.id,
                         "Address":thisData.Address,
                         "City":thisData.City,
                         "Description":thisData.Description,
@@ -35,13 +72,15 @@ export default function ResourceList(){
     });
 
     if (user && dbData) {
-        console.log(dbData);
         const listItems = dbData.map((doc) =>
-            <ListItem>Name: {doc.Name}. Address: {doc.Address}. Email: {doc.Email}</ListItem>
+            <ListItem button onClick={() => {handleClick(doc);}}>Name: {doc.Name}. Address: {doc.Address}. Email: {doc.Email}</ListItem>
         );
-        return <List>
-            {listItems}
-        </List>;
+        return <div>
+            <List>
+                {listItems}
+            </List>
+            <SimpleDialog open={open} onClose={handleClose} docInfo={selectedData} />
+        </div>;
     }
     else {
         return (
