996b721ec4f42004e0c26a33acb9cbf0c5d1b6a5
nwalzer <nswalzer@gmail.com>
2021-03-16 12:59:38 -0400

    tmp commit

--- frontend/src/components/UserMap.jsx
+++ frontend/src/components/UserMap.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import ResourceList from './ResourceList';
+import Button from '@material-ui/core/Button';
 
 import config from '../assets/mapbox.json'
 import mapboxgl from 'mapbox-gl';
@@ -26,6 +27,7 @@ export default function UserMap() {
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
+  const [mapObj, setMapObj] = useState();
 
   const [user, setUser] = useState(false);
   const [dbData, setDBData] = useState({});
@@ -134,20 +136,32 @@ export default function UserMap() {
 
       }
       );
+      setMapObj(map);
       return () => map.remove();
     }
     else return <div>Loading</div>
   }, [user, dbData]);
 
+  const handleFly = () => {
+    console.log("FLYING");
+    mapObj.flyTo({
+      center: [
+        -74.5 + (Math.random() - 0.5) * 10,
+        40 + (Math.random() - 0.5) * 10
+      ],
+      essential: true // this animation is considered essential with respect to prefers-reduced-motion
+    });
+  }
 
   return <div>
     <div class='sidebar'>
       <div class='heading'>
         <h1>Our locations</h1>
       </div>
+      <ResourceList />
+      <Button onclick={handleFly}>CLICK HERE FOR TEST</Button>
     </div>
     <div className="map-container" ref={mapContainer} />
-    <ResourceList/>
   </div>
 
 }
